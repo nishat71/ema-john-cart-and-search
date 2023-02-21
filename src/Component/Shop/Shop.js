88ca7615f3202f28efc3a2c4da983416e0cd88cd
Nishat <nishatbd17@gmail.com>
2023-02-21 16:14:47 +0600

    quantity problem solved

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -46,7 +46,7 @@ const Shop = () => {
     const savedCart = [];
     for (const id in storedCart) {
       const addedProduct = products.find(product => product.id === id);
-      if(addedProduct){
+      if (addedProduct) {
         const quantity = storedCart[id];
         addedProduct.quantity = quantity;
         savedCart.push(addedProduct);
@@ -58,16 +58,60 @@ const Shop = () => {
 
 
 
+  // //event handler
+  // const handleAddToCart = (selectedProduct) => {
+  //   console.log(selectedProduct);
+  //   let newCart = [];
+  //   const exists = cart.find(product => product.id === selectedProduct.id)
+  //   if (!exists) {
+  //     selectedProduct.quantity = 1;
+  //     newCart = [...cart, selectedProduct]; //jodi kuno item cart e na take, brand new item hoy tkn quantity 1 and new cart hobe cart er sb item plus new selected products
+  //   }
+  //   else {
+  //     const rest = cart.filter(product => product.id !== selectedProduct.id);  //j product add krbo seta bade rest gula ber krbo
+  //     exists.quantity = exists.quantity + 1;
+  //     newCart = [...rest, exists];
+  //     //j product add krbo seta bade rest gula ber krbo
+  //   }
+  //   // Do not do this : cart.push(product);
+  //   // const newCart = [...cart, selectedProduct];
+  //   setCart(newCart);
+  //   addToDb(selectedProduct.id);
+  //   //newCart nam e variable declare krlm ja age cart er modde joto item ase sb copy krbe, then akta parameter hisebe new product add krbo
+  //   // new product add korar por new  cart peye gesi tai setCart cll kore newCart pass kore dibo
+  // }
+
+
+
   //event handler
-  const handleAddToCart = (product) => {
-    console.log(product);
-    // Do not do this : cart.push(product);
-    const newCart = [...cart, product];
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const exists = cart.find(product => product.id === selectedProduct.id)
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    }
+    else {
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
-    addToDb(product.id);
-    //newCart nam e variable declare krlm ja age cart er modde joto item ase sb copy krbe, then akta parameter hisebe new product add krbo
-    // new product add korar por new  cart peye gesi tai setCart cll kore newCart pass kore dibo
+    addToDb(selectedProduct.id);
   }
+
+
+
+  // //event handler
+  // const handleAddToCart = (product) => {
+  //   console.log(product);
+  //     // Do not do this : cart.push(product);
+  //     const newCart = [...cart, product];
+  //   setCart(newCart);
+  //   addToDb(product.id);
+  // }
+
 
 
 
